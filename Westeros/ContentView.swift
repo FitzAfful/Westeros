206166378a4efccaf734ce15992b13554647d437
Fitzgerald Afful <fitzafful@gmail.com>
2022-10-25 15:40:30 +0000

    Initial Commit

---
+++ Westeros/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Westeros
+//
+//  Created by Fitzgerald Afful on 25/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Westeros/WesterosApp.swift
@@ -0,0 +1,17 @@
+//
+//  WesterosApp.swift
+//  Westeros
+//
+//  Created by Fitzgerald Afful on 25/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct WesterosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
