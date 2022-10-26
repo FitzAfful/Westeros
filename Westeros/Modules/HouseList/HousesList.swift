2d80e394e427dadbfdd54444b865fca718258c58
Fitzgerald Afful <fitzafful@gmail.com>
2022-10-26 16:22:30 +0000

    Create List for displaying list of houses

---
+++ Westeros/Modules/HouseList/HousesList.swift
@@ -0,0 +1,77 @@
+//
+//  HousesList.swift
+//  Westeros
+//
+//  Created by Fitzgerald Afful on 26/10/2022.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+struct HousesListView: View {
+    @ObservedObject var viewModel = HousesListViewModel()
+    @State var selectedHouse: String?
+
+    var body: some View {
+        NavigationView {
+            VStack(spacing: 0) {
+                if viewModel.isLoading {
+                    Spacer()
+                    ActivityIndicator()
+                    Spacer()
+                } else {
+                    List {
+                        Section {
+                        }
+
+                        ForEach(viewModel.filteredHouses) { house in
+                            NavigationLink {
+                                HouseDetailView(viewModel: HouseViewModel(house: house))
+                            } label: {
+                                HouseRow(house: house)
+                                    .onAppear {
+                                        viewModel.loadMore(currentHouse: house)
+                                    }
+                            }
+                        }
+                    }
+                    .padding(.top, 60)
+                }
+            }
+            .edgesIgnoringSafeArea(.top)
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    VStack {
+                        Text("Westeros")
+                            .defaultGOTFont(size: 28)
+                    }
+                }
+            }
+            .onAppear {
+                viewModel.loadHouses()
+            }
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
+    }
+}
+
+struct HouseRow: View {
+    var house: House
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            Text(house.name)
+                .lineLimit(1)
+                .font(.title3)
+            Spacer()
+                .frame(height: 2)
+
+            Text(house.region)
+                .lineLimit(1)
+                .font(.body)
+                .foregroundColor(.gray)
+        }
+    }
+}
